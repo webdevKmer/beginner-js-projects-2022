f5cec81dc44a7a0e8f8d99545d088dda9c2c9b83
webdevKmer <webdevKmer@gmail.com>
2022-11-20 15:10:16 +0000

    fixing styling and finishing functionnality

--- slider.js
+++ slider.js
@@ -1,1 +1,43 @@
-console.log("it works");
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const slider = document.querySelector('.slider')
+
+console.log(left, right);
+
+const all_imgs = document.querySelectorAll('.image')
+
+let slide_pos = 0
+
+function getNextPos(){
+    return -500 * slide_pos
+}
+function getPrevPos(){
+    return -500 * (slide_pos-1)
+}
+
+function nextSlide(){
+    if (slide_pos >= all_imgs.length - 1){
+        slide_pos = -1
+    }
+    slide_pos += 1
+    new_pos = getNextPos()
+    slider.style.transform = `translate(${new_pos}px)`
+    console.log(slide_pos, new_pos);
+}
+function prevSlide(){
+    if (slide_pos <= 1){
+        slide_pos = all_imgs.length
+    }
+    slide_pos -= 1
+    new_pos = getPrevPos()
+    slider.style.transform = `translate(${new_pos}px)`
+    console.log(slide_pos, new_pos);
+}
+
+left.addEventListener('click', () => {
+    prevSlide()
+    
+})
+right.addEventListener('click', () => {
+    nextSlide()
+})
